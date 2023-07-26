af0225e6a2d1230fc6b953d2e7f6af919ce0c6bf
Stéphane Klein <contact@stephane-klein.info>
2023-07-26 17:16:04 +0200

    Add nodemailer package and send mail to maildev

---
+++ src/routes/+page.server.js
@@ -0,0 +1,17 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+    host: "127.0.0.1",
+    port: 1025,
+    secure: false
+});
+
+const info = await transporter.sendMail({
+    from: "\"Fred Foo 👻\" <foo@example.com>",
+    to: "bar@example.com, baz@example.com",
+    subject: "Hello ✔",
+    text: "Hello world?",
+    html: "<b>Hello world?</b>"
+});
+
+console.log("Message sent: %s", info.messageId);
